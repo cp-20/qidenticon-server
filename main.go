d732c5489cb7341af669aeaa27a47a467a37969b
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2024-04-26 11:36:15 +0900

    `.png` suffix やめた & md5を返せるように

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
 	"image/png"
 	"log"
 	"net/http"
@@ -17,23 +18,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	args := strings.Split(r.URL.Path, "/")
 	args = args[1:]
 
-	if len(args) != 1 {
+	if len(args) == 1 || (len(args) == 2 && args[1] == "md5") {
 		http.Error(w, "", http.StatusBadRequest)
 		return
 	}
 
 	item := args[0]
 
-	// support jpg too?
-	if !strings.HasSuffix(item, ".png") {
-		http.Error(w, "", http.StatusBadRequest)
-		return
-	}
-
-	item = strings.TrimSuffix(item, ".png")
-
 	code := qidenticon.Code(item)
-	log.Println("code:", code)
 	size := 256
 	settings := qidenticon.DefaultSettings()
 
@@ -41,17 +33,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	img := qidenticon.Render(code, size, settings)
 
-	log.Printf("creating identicon for '%s'\n", item)
-
 	buffer := new(bytes.Buffer)
 	if err := png.Encode(buffer, img); err != nil {
 		log.Println("unable to encode image.")
 	}
 
-	w.Header().Set("Content-Type", "image/png")
-	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
-	if _, err := w.Write(buffer.Bytes()); err != nil {
-		log.Println("unable to write image.")
+	if len(args) == 1 {
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+		if _, err := w.Write(buffer.Bytes()); err != nil {
+			log.Println("unable to write image.")
+		}
+		return
+	}
+
+	md5 := md5.New().Sum(buffer.Bytes())
+
+	w.Header().Set("Content-Type", "plain/text")
+	w.Header().Set("Content-Length", strconv.Itoa(len(md5)))
+	if _, err := w.Write(md5); err != nil {
+		log.Println("unable to write md5")
 	}
 }
 
